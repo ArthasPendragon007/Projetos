c4c7e5a900c9a534a6394061c169583f61bb9ebc
ArthasPendragon007 <93739412+ArthasPendragon007@users.noreply.github.com>
2024-11-07 20:12:38 -0300

    commit 1

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css'; // Importa o arquivo de estilos
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Bem-vindo à Minha Página React!</h1>
+        <p>Este é um exemplo básico de uma aplicação React.</p>
+        <button className="btn">Clique aqui</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css'; // Estilos globais
+import App from '.'; // O componente principal
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
